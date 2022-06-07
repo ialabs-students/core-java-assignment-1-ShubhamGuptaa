67673e108404dad34cc633360b2cffcb13d4ba3a
Shubham Kumar <shubham.kumar@atcs.com>
2022-06-07 12:40:42 +0530

    bug fixed

--- assignment_1/Product.java
+++ assignment_1/Product.java
@@ -91,21 +91,21 @@ public class Product {
         // calling the method findMaxPriceProduct from Product service and pass the
         // argument catagory as type string!
         // It will return product object
-        // Product product_obj = productService.findMaxPriceProduct("mobile");
-        // System.out.println(product_obj);
+        Product product_obj = productService.findMaxPriceProduct("mobile");
+        System.out.println(product_obj);
 
         // calling the method getProductsByCatagory from Product service and pass the
         // argument catagory as type string!
         // it will return the list of products
-    //     ArrayList<Product> products = productService.getProductsByCategory("fashion");
-    //     if (products != null) {
-    //         Iterator itr = products.iterator();
-    //         while (itr.hasNext()) {
-    //             Product product = (Product) itr.next();
-    //             System.out.println("[ product code: " + product.getProductCode() + ", product name: "
-    //                     + product.getName() + ", product price: Rs." + product.getPrice() + ", product catagory: "
-    //                     + product.getCategory() + " ]");
-    //         }
-    //     }
+        ArrayList<Product> products = productService.getProductsByCategory("fashion");
+        if (products != null) {
+            Iterator itr = products.iterator();
+            while (itr.hasNext()) {
+                Product product = (Product) itr.next();
+                System.out.println("[ product code: " + product.getProductCode() + ", product name: "
+                        + product.getName() + ", product price: Rs." + product.getPrice() + ", product catagory: "
+                        + product.getCategory() + " ]");
+            }
+        }
     }
 }
